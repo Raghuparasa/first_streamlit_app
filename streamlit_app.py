50f9bafd7e5139dea74d618a98599ba7d9c37f60
Raghuparasa <137636407+Raghuparasa@users.noreply.github.com>
2023-06-28 07:36:09 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,3 +20,7 @@ fruites_to_show = my_fruit_list.loc[fruits_selected]
 
 #display the table on page
 streamlit.dataframe(fruites_to_show)
+
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
