796135b6e3fa4e41a1a45eda45f3de415933f059
Raghuparasa <137636407+Raghuparasa@users.noreply.github.com>
2023-06-28 23:51:47 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,7 +45,7 @@ except URLError as e:
 #take the above json format data and normalize it and display
 
 
-streamlit.header('The Fruit load list contains')
+streamlit.header('View Our Fruit List- Add Your Favorites!')
 def get_fruit_load_list():
    with  my_cnx.cursor() as my_cur:
          my_cur.execute("SELECT * from fruit_load_list")
